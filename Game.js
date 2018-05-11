2fc0454d568ea15603c93247990b5f2eec3afea5
Aaron G <aaronhgross@gmail.com>
2018-05-11 15:01:06 -0400

    changed to class

--- Game.js
+++ Game.js
@@ -1,14 +1,48 @@
 class Game {
     constructor() {
-    this.users = []
-    this.countdown = null
+    this.users = [],
+    this.countdown = null,
+    this.numOfUsers = 0
     }
 
     addUser(user) {
-        let newusers = this.users.slice();
-        newusers.push(user)
-        this.users = newusers;
+        let newUsers = this.users.slice();
+        newUsers.push(user)
+        this.users = newUsers;
+        console.log('addUser:',this.users)
+    }
+
+    removeUser(id) {
+        console.log(`Disconnected ${id}`)
+        let removedUser = this.users.filter( user => user.id !== id)
+        this.users = removedUser;
+    }
+
+    changeReadyStatus(id, status) {
+        let newUsers = this.users.slice();
+        let theuser = newUsers.find((user) => user.id === id)
+        theuser.ready = status;
+        this.users = newUsers;
+        if(status === true) {
+            this.numOfUsers++
+          } else {
+            this.numOfUsers--
+          }
+          console.log('number of users:', this.numOfUsers)
+    }
+
+    startCountdown() {
+        for (let user of this.users) {
+            if(this.numOfUsers === this.users.length){
+              console.log('all users ready')
+            //   countDown(socket, true)
+            }
+            else {
+              console.log('users are not ready')
+            //   countDown(socket, false)
+            }
+          }
     }
 }
 
-module.exports= Game
+module.exports = Game
--- server.js
+++ server.js
@@ -2,102 +2,51 @@ const io = require('socket.io')();
 const Game = require('./Game')
 const port = 8000;
 
-let users = [];
-let i = 0;
-// randomize a room name?
-
 let game = new Game();
 
-let interval;
-
-let countDown = (socket, status) => {
-    console.log('status', status)
-  let num = 10;
-  if (status === true) {
-    interval = setInterval( () => {
-      if (num >= 0) {
-      io.emit('countdown-numbers', num)
-      num--;
-        } 
-      }, 1000)
-  } else if (status === false) { 
-    clearInterval(interval)
-    interval = null;
-    io.emit('countdown-numbers', 'false')
-    }
-}
-  
-//  if (status === true) {
-//   interval(num);
-//  } else if (status === false) {
-//     clearInterval(interval())
-//     io.emit('countdown-numbers', 'false')
-//  }
-
+// let interval;
 
 // let countDown = (socket, status) => {
-//   console.log('status', status)
-//  if (status === true) {
-//     io.emit('countdown-numbers', 10)
-//  } else if (status === false) {
-//     io.emit('countdown-numbers', '')
-//  }
+//     console.log('status', status)
+//   let num = 10;
+//   if (status === true) {
+//     interval = setInterval( () => {
+//       if (num >= 0) {
+//       io.emit('countdown-numbers', num)
+//       num--;
+//         } 
+//       }, 1000)
+//   } else if (status === false) { 
+//     clearInterval(interval)
+//     interval = null;
+//     io.emit('countdown-numbers', 'false')
+//     }
 // }
-
-// let countDown = (socket, status) => { 
-//   console.log('status', status)
-//  if (status === true) {
-//   io.emit('countdown-numbers', 'true')
-//  } else if (status === false) {
-//   io.emit('countdown-numbers', 'false')
-
-//  }
-// }
-
+ 
 io.on('connection', (socket) => {
   console.log('Socket id: ',socket.id)
-  io.emit('current-users', users)
+  // console.log(game.users)
+  io.emit('current-users', game.users)
   socket.emit('clientid', socket.id)
   
-  socket.on('join-game', (user) => {
-    // users.push(user)
+  socket.on('join-game', (user) => { 
     game.addUser(user)
     io.emit('current-users', game.users)
   })
 
   socket.on('user-ready', (id, status) => {
-    let theuser = users.find((user) => user.id === id)
-    theuser.ready = status;
-    if(status === true) {
-      i++
-    } else {
-      i--
-    }
-    console.log('number of users:', i)
-    io.emit('current-users', users)
-
-    for (let user of users) {
-        if(i === users.length){
-          console.log('all users ready')
-          countDown(socket, true)
-        }
-        else {
-          console.log('users are not ready')
-          countDown(socket, false)
-        }
-      }
+    game.changeReadyStatus(id, status)
+    io.emit('current-users', game.users)
+    game.startCountdown()
     })
 
   socket.on('disconnect', () => {
-    console.log(`Disconnected ${socket.id}`)
-    let removeduser = users.filter( user => user.id !== socket.id)
-    users = removeduser;
-    io.emit('current-users', users)
+    game.removeUser(socket.id)
+    io.emit('current-users', game.users)
   })
 
   socket.on('test', () => console.log('Test worked!'))
 })
-
 
 io.listen(port);
 console.log('Listening on port ', port);
