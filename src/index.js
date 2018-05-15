347f34fac3b2ffc9fab1ed5e118e5fec5ba1fb21
Aaron G <aaronhgross@gmail.com>
2018-05-15 14:29:59 -0400

    port

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import { UPDATE_USERNAME,
         UPDATE_CHOICE_COUNTDOWN,
         RESET_STATE } from './actions/users'
 
-const socket = openSocket('https://floating-anchorage-15230.herokuapp.com');
+const socket = openSocket('https://floating-anchorage-15230.herokuapp.com:41144');
 
 let initialState = {
     users: [],
